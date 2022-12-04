d04a5f5485835139a0d8f2898b60ae20fb787170
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-04 10:26:56 -0800

    need to recast as int

--- day4/workout.py
+++ day4/workout.py
@@ -1,4 +1,4 @@
-f = open('testcase')
+f = open('input')
 lines = f.readlines(-1)
 
 overlaps = 0
@@ -7,6 +7,10 @@ while lines:
     (elfA, elfB) = lines.pop().strip().split(',')
     (aStart, aEnd) = elfA.split('-')
     (bStart, bEnd) = elfB.split('-')
+    aStart = int(aStart)
+    aEnd = int(aEnd)
+    bStart = int(bStart)
+    bEnd = int(bEnd)
 
     if aStart <= bStart:
         if aEnd >= bEnd:

d90b0e4879dd1ceaee7ab5404c954b7179404f93
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-06 11:01:29 -0800

    testcase passing

---
+++ day6/signals.py
@@ -0,0 +1,9 @@
+signal = open('testcase').readline()
+for i in range(len(signal)):
+  chunk = signal[i:i+4]
+  chunkset = {j for j in chunk}
+  if len(chunkset) == 4:
+      print(i)
+      break
+
+

8ad86f92bc172795aed729d6d79616864dd85fdb
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-07 15:12:36 -0800

    seem to be building tree correctly

--- day7/trees.py
+++ day7/trees.py
@@ -2,8 +2,8 @@ import anytree
 
 
 lines = open('testcase').readlines(-1)
-dirs = {'root': anytree.Node('root')}
-cur = dirs['root']
+dirs = {'root': [anytree.Node('root'),0]}
+cur = dirs['root'][0]
 
 
 for l in lines:
@@ -20,18 +20,19 @@ for l in lines:
             continue
         if dest == '/':
             continue
-        cur = dirs[dest]
+        cur = dirs[dest][0]
 
     if parts[0] == 'dir':
+        print(dirs)
         dirname = parts[1]
         print("looking at %s, parent is %s" % (dirname, cur.name))
-        dirs[dirname] = anytree.Node(dirname, parent=cur)
-        
+        dirs[dirname] = []
+        dirs[dirname].append(anytree.Node(dirname, parent=cur))
+        dirs[dirname].append(0)
 
 
-for pre, fill, node in anytree.RenderTree(dirs['root']):
+for pre, fill, node in anytree.RenderTree(dirs['root'][0]):
     print("%s%s" % (pre, node.name))
 
-print(dirs)
 for d in dirs.values():
-    print(d.parent)
+    print(d[0].parent)

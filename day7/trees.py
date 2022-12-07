111e01c3a5d67ec75d9693986faa1ac73c025a59
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-07 15:06:42 -0800

    using a tree module instead of making my own

--- day7/trees.py
+++ day7/trees.py
@@ -1,18 +1,37 @@
-class Dir:
-    def __init__():
-        contents = []
-
-    def add(name, size=0):
-        contents.append({'Name': name, 'Size': size})
-
-    def calc():
-        size = 0
-        for o in contents:
-            if isinstance(o, Dir):
-                o['Size'] = o.calc()
-            size += o['Size']
-
+import anytree
 
 
 lines = open('testcase').readlines(-1)
+dirs = {'root': anytree.Node('root')}
+cur = dirs['root']
 
+
+for l in lines:
+    parts = l.strip().split()
+    if parts[0].isnumeric():
+        continue # skipping files for now
+    
+    if parts[1] == 'ls':
+        continue # don't think we need to do anything here
+
+    if parts[1] == 'cd':
+        dest = parts[2]
+        if dest == '..':
+            continue
+        if dest == '/':
+            continue
+        cur = dirs[dest]
+
+    if parts[0] == 'dir':
+        dirname = parts[1]
+        print("looking at %s, parent is %s" % (dirname, cur.name))
+        dirs[dirname] = anytree.Node(dirname, parent=cur)
+        
+
+
+for pre, fill, node in anytree.RenderTree(dirs['root']):
+    print("%s%s" % (pre, node.name))
+
+print(dirs)
+for d in dirs.values():
+    print(d.parent)

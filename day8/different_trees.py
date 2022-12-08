15ec9447ba36cfd9ed144163cf9f9d56264675d3
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-08 15:44:01 -0800

    perimeter not set as visible for some reason

--- day8/different_trees.py
+++ day8/different_trees.py
@@ -1,17 +1,53 @@
+from copy import deepcopy
+
 lines = open('testcase')
 grid = []
+
 
 for l in lines:
     grid.append(list(l.strip()))
 
-def get_col(a, col):
+visible = deepcopy(grid)
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        visible[i][j] = False
+
+def get_col(a, c):
     col = []
     for r in a:
-        col.apend(r)
+        col.append(r[c])
     return col
 
 def get_row(a,row):
     return row[a]
 
+for i in range(len(grid)):
+    visible[i][0] = True  # first and last items are always visible
+    visible[i][-1] = True
+    for j in range(1,len(grid[i])):
+        if grid[i][j] > max(grid[i][0:j]): # current item is larger than the tallest thing to the left
+            visible[i][j] = True 
+    for j in range(len(grid[i])-1):
+        if grid[i][j] > max(grid[i][j+1:]): # current item is larger than the tallest thing to the right
+            visible[i][j] = True
+
+for i in range(len(grid[0])):
+    col = get_col(grid, i)
+    for j in range(1,len(col)):
+        if col[j] > max(col[0:j]):
+            visible[i][j] = True
+    for j in range(len(col)-1):
+        if col[j] > max(col[j+1:]):
+            visible[i][j] = True
+
+for row in visible:
+    thisrow = ''
+    for col in row:
+        if col:
+            thisrow += 'T'
+        else:
+            thisrow += ' '
+    print(thisrow)
 
 

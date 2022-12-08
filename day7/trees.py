b1f3bc561d1c44bd7419d88e883cd900e32704b0
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-07 18:59:03 -0800

    part 2 test case passing

--- day7/trees.py
+++ day7/trees.py
@@ -9,7 +9,7 @@ fs = []
 parent = None
 cur = None
 
-lines = open('input').readlines(-1)
+lines = open('testcase').readlines(-1)
 
 for l in lines:
     parts = l.strip().split()
@@ -50,8 +50,20 @@ backfill(fs[0])
 
 smalldirs = 0
 for f in fs:
-    if f.size <= 100000 and f.children:
+    if f.size <= 100000 and len(f.children) > 0:
       smalldirs += f.size
 
 print(smalldirs)
+
+avail = 70000000 - fs[0].size
+needed = 30000000 - avail
+
+smallest = fs[0]
+
+for f in fs:
+    if f.size >= needed and len(f.children) > 0:
+        if f.size < smallest.size:
+            smallest = f
+
+print(smallest.name)
 

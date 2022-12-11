0dc0ef7ad20f5bd65069d82dee2165386ac6ce13
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-10 20:21:06 -0800

    building grid and setting start

--- day9/planck.py
+++ day9/planck.py
@@ -12,4 +12,23 @@ for l in lines:
     elif dir == 'D':
         cur_y -= num
         below = min(cur_y, below)
+    elif dir == 'R':
+        cur_x += num
+        right = max(cur_x, right)
+    elif dir == 'L':
+        cur_x -= num
+        left = min(cur_x, left)
+
+print(left, right)
+print(above, below)
+
+grid = [ [False for i in range(left+right+1)] for j in range(above+below+1) ]
+head_x = right
+head_y = above
+tail_x = right
+head_y = above
+grid[above][left] = True
+
+for i in grid:
+    print(i)
     

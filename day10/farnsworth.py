85eb3ff47c9bdf1875a8edee678d9c69e48dc1c5
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-23 09:31:59 -0800

    mostly working but not starting a new line when it should sometimes

--- day10/farnsworth.py
+++ day10/farnsworth.py
@@ -1,26 +1,51 @@
-lines = open('input').readlines(-1)
+lines = open('testcase').readlines(-1)
 
 x = 1
 clock = 0
 sum = 0
+pixels = ''
+
+def overlap(clock, reg):
+    clock = clock % 40
+    if abs(clock-reg) <= 1:
+        return True
+    else:
+        return False
 
 for l in lines:
     if l[0:4] == 'noop':
+        if overlap(clock, x):
+            pixels += '#'
+        else:
+            pixels += '.'
         clock += 1
         if clock % 40 == 20:
-            print(l, clock, x)
             sum += x * clock
+        elif clock % 40 == 0:
+            print(pixels)
+            pixels = ''
     else:
         arg = int(l.split()[1])
+        if overlap(clock, x):
+            pixels += '#'
+        else:
+            pixels += '.'
         clock += 1
         if clock % 40 == 20:
-            print(l, clock, x)
             sum += x * clock
+        elif clock % 40 == 0:
+            print(pixels)
+            pixels = ''
+        if overlap(clock, x):
+            pixels += '#'
+        else:
+            pixels += '.'
         clock += 1
-        
         if clock % 40 == 20:
-            print(l, clock, x)
             sum += x * clock
+        elif clock % 40 == 0:
+            print(pixels)
+            pixels == ''
         x += arg
 
 print(sum)

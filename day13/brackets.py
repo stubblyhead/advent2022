3e846879f52c751a902ebc9a422242bb430f25f4
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2022-12-31 17:01:35 -0800

    comparator function not handling some cases correctly, may be easier to redo than rehab

--- day13/brackets.py
+++ day13/brackets.py
@@ -1,11 +1,14 @@
 from copy import deepcopy
+from functools import cmp_to_key
 
 with open('input') as f:
     data = f.read()
 
 pairs = data.split('\n\n')
 
-def cmp(l, r):
+def cmp(left, right):
+    l = deepcopy(left)
+    r = deepcopy(right)
     result = 0  # pass through until we know we can do otherwise
     was_zero = False # need to do some special handling if LHS == 0
     if l == 0:  
@@ -41,11 +44,14 @@ def cmp(l, r):
     
 
 sum = 0
-
+allpairs = []
 for i in range(len(pairs)):
     (left, right) = pairs[i].split('\n')
+   
     left = eval(left)  # this seems like cheating, but probably will come back to bite me
     right = eval(right)
+    allpairs.append(left) # will need these for part 2, collecting them now is convenient
+    allpairs.append(right)
     
     thispair = cmp(deepcopy(left),deepcopy(right))
     if thispair == 1:
@@ -53,3 +59,10 @@ for i in range(len(pairs)):
         print("pair %i\n%s comes before %s" % (i,left,right))
 
 print(sum)
+
+allpairs.append([[2]])
+allpairs.append([[6]]) # 99% sure this one is going to be a problem
+
+sortedpairs = sorted(allpairs, key=cmp_to_key(cmp))
+
+print((sortedpairs.index([[2]]) + 1) * (sortedpairs.index([[6]]) + 1))
